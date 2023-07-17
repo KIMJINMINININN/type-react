07008a181ada9ad382869b3fbe94b3783efda9ae
김진민 <kjm9596@douzone.com>
2023-07-17 16:11:01 +0900

    recoil 환경설정 및 테스트 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil';
+import Counter from "./component/Counter";
+import { counterState, counterLabelSelector } from './state/store';
+
 const App = () => {
-    return <h1>Hello World!</h1>;
+    const counter = useRecoilValue(counterState);
+    const counterLabel = useRecoilValue(counterLabelSelector);
+
+    return (
+        <RecoilRoot>
+            <Counter />
+            <div>counter : {counter}</div>
+            <div>counterLabel : {counterLabel}</div>
+        </RecoilRoot>
+    );
 };
 ReactDOM.render(<App />, document.getElementById('app'));
